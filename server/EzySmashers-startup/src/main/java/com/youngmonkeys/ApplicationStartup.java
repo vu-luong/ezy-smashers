4f14d4002e6b766295db7cfdd2c360ca19caa4a3
vu-luong <vubinhcht@gmail.com>
2021-10-13 20:56:58 +1000

    Activate websocket

--- server/EzySmashers-startup/src/main/java/com/youngmonkeys/ApplicationStartup.java
+++ server/EzySmashers-startup/src/main/java/com/youngmonkeys/ApplicationStartup.java
@@ -35,7 +35,7 @@ public class ApplicationStartup {
 				.plugin(pluginSettingBuilder.build());
 		
 		EzyWebSocketSettingBuilder webSocketSettingBuilder = new EzyWebSocketSettingBuilder()
-				.active(false);
+				.active(true);
 		
 		EzyUdpSettingBuilder udpSettingBuilder = new EzyUdpSettingBuilder()
 				.active(true);
--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,4 @@
-#export EZYFOX_SERVER_HOME=
+export EZYFOX_SERVER_HOME=/home/vuluong/Work/youngmonkeys/dev/EzySmashers/ezyfox-server
 mvn -pl . clean install
 mvn -pl EzySmashers-common -Pexport clean install
 mvn -pl EzySmashers-app-api -Pexport clean install
